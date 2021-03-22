c0a86cc25a1dffa23b3397bf7a9c62bc8c8fed29
Gregory <62161730+GregJans@users.noreply.github.com>
2021-03-22 11:22:13 -0400

    Update Atom.py
    removed useless code in the Atom file

--- Atom.py
+++ Atom.py
@@ -11,23 +11,3 @@ def fib(n):
 
 
 print(fib(12))
-
-
-# tries to solve past problems by storing known fib()...makes it more efficient
-# recursive approach
-def mfib(n, memo):
-    if memo[n] != null:  # not sure if pyton has null
-        return memo[n]
-    if n == 1 or n == 2:
-        result = 1
-    else:
-        result = mfib(n-1) + mfib(n-2)
-        memo[n] = result
-    return result
-
-
-# will not actually work without memo...not sure how it works
-print(mfib(5))
-
-
-# NOTE: look up Bitwise opperators           https://www.geeksforgeeks.org/basic-operators-python/
